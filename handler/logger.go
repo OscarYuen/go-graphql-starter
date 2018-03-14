df0fcdc2e406a6f5e0bd0a8bd99b9b88c99b79f7
Oscar Yuen <oscaryuen@gmail.com>
2018-03-14 17:06:57 +0800

    Update logger.go

--- handler/logger.go
+++ handler/logger.go
@@ -14,7 +14,7 @@ type LoggerHandler struct {
 func (l *LoggerHandler) Logging(h http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		ctx := r.Context()
-		log := ctx.Value("userService").(*logging.Logger)
+		log := ctx.Value("log").(*logging.Logger)
 		log.Infof("%s %s %s %s", r.RemoteAddr, r.Method, r.URL, r.Proto)
 		log.Infof("User agent : %s", r.UserAgent())
 		if l.DebugMode {
